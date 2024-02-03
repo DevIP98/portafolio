849656b55fb1e19ada484a418d2327932aa778e4
Iván Pineda <ip1805x@gmail.com>
2024-02-03 14:48:19 -0500

    Finish class 13

--- resources/views/livewire/navigation/navigation.blade.php
+++ resources/views/livewire/navigation/navigation.blade.php
@@ -26,9 +26,19 @@
                     </div>
                 </div>
                 <div class="hidden md:block md:ml-10 md:pr-4 md:space-x-8 text-lg">
-                    <x-navigation.links class="text-gray-200 hover:text-red-300" :items="$items"/>
+                    <x-navigation.links class="text-gray-200 hover:text-red-300" :items="$items" />
                 </div>
                 <!-- Aqui botones edit y add -->
+                {{-- @auth --}}
+                <div class="hidden md:flex items-center justify-between space-x-2 ml-4 pb-1">
+                    <x-actions.action class="text-yellow-300 hover:text-blue-300" title="{{ __('Edit') }}">
+                        <x-icons.edit />
+                    </x-actions.action>
+                    <x-actions.action class="text-yellow-300 hover:text-blue-300" title="{{ __('New') }}">
+                        <x-icons.add />
+                    </x-actions.action>
+                </div>
+                {{-- @endauth --}}
             </nav>
         </div>
 
@@ -57,12 +67,23 @@
                     </div>
                 </div>
                 <div class="px-2 pt-2 pb-3 space-y-1">
-                    <x-navigation.links class="block px-3 py-2 rounded-md text-base text-gray-700 hover:text-gray-900 hover:bg-gray-50" :items="$items"/>
+                    <x-navigation.links
+                        class="block px-3 py-2 rounded-md text-base text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                        :items="$items" />
                     <!-- Aqui botones edit y add -->
+                    {{-- @auth --}}
+                    <x-actions.action class="block px-2 text-yellow-500 hover:text-blue-500"
+                        title="{{ __('Edit') }}">
+                        <x-icons.edit />
+                    </x-actions.action>
+                    <x-actions.action class="block px-2 text-yellow-500 hover:text-blue-500"
+                        title="{{ __('New') }}">
+                        <x-icons.add />
+                    </x-actions.action>
+                    {{-- @endauth --}}
                 </div>
             </div>
         </div>
     </div>
-
     <!-- slideover add/edit -->
 </section>
